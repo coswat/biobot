17a495ed05d4e2d2c9771e8c8f801e024e5bddf4
coderswat <abhishek.b4696@gmail.com>
2023-08-18 22:56:35 +0530

    formatted main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,27 +12,25 @@ use teloxide::Bot;
 //     pretty_env_logger::init();
 //     log::info!("Starting bio bot...");
 //     dotenv().ok();
-// 
+//
 //     let bot: Bot = Bot::from_env();
-// 
+//
 //     let port: u16 = env::var("PORT")
 //         .expect("PORT env variable is not set")
 //         .parse()
 //         .expect("PORT env variable value is not an integer");
-// 
+//
 //     let addr = ([0, 0, 0, 0], port).into();
-// 
+//
 //     let host = env::var("HOST").expect("HOST env variable is not set");
 //     let url = format!("https://{host}/webhook").parse().unwrap();
-// 
+//
 //     let listener = webhooks::axum(bot.clone(), webhooks::Options::new(addr, url))
 //         .await
 //         .expect("Couldn't setup webhook");
-// 
+//
 //     teloxide::repl_with_listener(bot, bothandler::init, listener).await;
 // }
-
-
 
 //FOR DEVELOPMENT USE THIS MAIN FUNCTION INSTEAD OF THE ABOVE ONE
 
@@ -46,5 +44,3 @@ async fn main() {
 
     teloxide::repl(bot, bothandler::init).await;
 }
-
-
